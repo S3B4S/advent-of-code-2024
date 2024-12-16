06cc5266dbe3316dfbafed7fbbed526bc6b31d4f
Kevin van der Werff <kevin.vanderwerff@deptagency.com>
2024-12-16 22:35:16 +0100

    fix lint

--- 16_reindeer-maze/index.ts
+++ 16_reindeer-maze/index.ts
@@ -6,7 +6,6 @@ import {
   turn90DegreesCounterClockwise,
   destringifyCoordDirection,
 } from "../utils/board.ts";
-import { equalCoordinates } from "../utils/board.ts";
 import { stringifyCoord } from "../utils/board.ts";
 import { Board, Direction } from "../utils/board.ts";
 import { HashSet } from "../utils/hashSet.ts";
@@ -99,7 +98,6 @@ const getAllCoords = (
 
 const traverseBackwardsForPaths = (
   mem: Record<string, { cost: number; lastNodes: string[] }>,
-  start: Coordinate,
   end: Coordinate,
   lowestCostToEnd: number
 ) => {
@@ -123,12 +121,6 @@ const traverseBackwardsForPaths = (
 
     while (queue.length > 0) {
       const currentNode = queue.shift()!;
-      // console.log("--------");
-      // console.log(
-      //   "currentNode",
-      //   currentNode,
-      //   stringifyCoordDirection(currentNode.coord, currentNode.dir)
-      // );
       const memKey = stringifyCoordDirection(
         currentNode.coord,
         currentNode.dir
@@ -293,135 +285,6 @@ const constructDirectedWeightedGraph = (
   return graph;
 };
 
-const findPaths = (
-  board: Board<string, number>,
-  start: Coordinate,
-  end: Coordinate
-) => {
-  const queue: Coordinate[] = [];
-  queue.push(start);
-
-  const mem = {} as Record<
-    string,
-    {
-      known: boolean;
-      cost: number;
-      lastNode: Coordinate;
-    }
-  >;
-
-  mem[stringifyCoord(start)] = {
-    known: false,
-    cost: 0,
-    lastNode: start,
-  };
-  // const visited = new HashSet<Coordinate>(stringifyCoord);
-  // visited.include(start);
-
-  const finalPaths: Coordinate[][] = [];
-
-  while (queue.length > 0) {
-    const currentNode = queue.shift()!;
-
-    mem[stringifyCoord(currentNode)] = {
-      known: true,
-      cost: mem[stringifyCoord(currentNode)].cost + 1,
-      lastNode: currentNode,
-    };
-
-    // const log = (msg: unknown) => {
-    //   if (currentPath.path.find((c) => c.col === 10 && c.row === 7)) {
-    //     console.log("cond log:", msg);
-    //   }
-    // };
-    // const lastNode = currentPath.path[currentPath.path.length - 1];
-
-    // log(currentPath);
-    // log(lastNode);
-
-    // if (equalCoordinates(currentNode, end)) {
-    // log("pushing to final !!!!!");
-    // log(finalPaths);
-    // finalPaths.push(currentPath.path);
-    // continue;
-    // return currentPath;
-    // }
-
-    for (const neighbor of board.getNeighbours(currentNode, [
-      Direction.N,
-      Direction.E,
-      Direction.S,
-      Direction.W,
-    ])) {
-      if (
-        board.safeGetCell(neighbor) !== "." &&
-        board.safeGetCell(neighbor) !== "E"
-      )
-        continue;
-
-      // console.log(currentNode);
-      // if (!currentNode.contains(neighbor)) {
-      // if (lastNode.col === 9 && lastNode.row === 7) {
-      // console.log("9,7 nb:", neighbor);
-      // }
-
-      // Do not add the end coordinate to visited
-      // Might want to keep track of visited per path
-      // if (!equalCoordinates(neighbor, end))
-      // currentPath.visited.include(neighbor);
-
-      queue.push(neighbor);
-      // if (lastNode.col === 9 && lastNode.row === 7) {
-      // console.log("9,7 queue:", queue);
-      // }
-      // }
-    }
-  }
-
-  return finalPaths;
-};
-
-const calculatePath = (path: Coordinate[]): { step: number; turn: number } => {
-  // Whether we are traversing a column or a row
-  let traversingCol = path[0].col === path[1].col;
-  const count = {
-    step: 0,
-    turn: 0,
-  };
-
-  for (let i = 1; i < path.length; i++) {
-    const prev = path[i - 1];
-    const current = path[i];
-
-    let action;
-    if (traversingCol && prev.col === current.col) {
-      // We are still traversing the same column
-      action = "travel-same-col";
-      count.step += +1;
-    } else if (traversingCol && prev.row === current.row) {
-      // We have taken a turn and a step
-      action = "turn-from-col";
-      count.turn += 1;
-      count.step += 1;
-      traversingCol = !traversingCol;
-    } else if (!traversingCol && prev.row === current.row) {
-      action = "travel-same-row";
-      // We are still traversing the same row
-      count.step += 1;
-    } else {
-      action = "turn-from-row";
-      // We have taken a turn and a step
-      count.turn += 1;
-      count.step += 1;
-      traversingCol = !traversingCol;
-    }
-
-    // console.log(prev, current, action);
-  }
-
-  return count;
-};
-
 export const solvePart2 = (input: string) => {
   const board = Board.fromUnparsedBoard(input);
 
@@ -436,7 +299,6 @@ export const solvePart2 = (input: string) => {
   );
   const allTiles = traverseBackwardsForPaths(
     dijkstraGraph,
-    start,
     end,
     lowestCostToEnd
   );
