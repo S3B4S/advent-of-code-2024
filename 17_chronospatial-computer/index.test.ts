36a6f0b4d9b29102692c132280af2c9dbe7be086
Kevin van der Werff <kevin.vanderwerff@deptagency.com>
2024-12-17 14:15:29 +0100

    day 17 part 1

---
+++ 17_chronospatial-computer/index.test.ts
@@ -0,0 +1,243 @@
+import { expect } from "jsr:@std/expect";
+import { Programs, solvePart1, solvePart2 } from "./index.ts";
+import { testWrapper } from "../utils/misc.ts";
+import deno from "https://deno.land/x/y18n@v5.0.0-deno/lib/platform-shims/deno.ts";
+
+let fileInput: string;
+try {
+  fileInput = Deno.readTextFileSync("./17_chronospatial-computer/input.txt");
+} catch {
+  // In GitHub Actions the `Deno.readTextFileSync` will fail, as the `input.txt` is not committed to git.
+  // So we assign an empty string, knowing that in GitHub Actions the fileInput won't be used anyway.
+  fileInput = "";
+}
+
+const exampleInput = `
+  Register A: 729
+  Register B: 0
+  Register C: 0
+
+  Program: 0,1,5,4,3,0
+`
+  .replaceAll(/^ +/gm, "")
+  .trim();
+
+testWrapper("Day 17", () => {
+  Deno.test("Part 1 - Example input", () => {
+    expect(solvePart1(exampleInput).outcome).toEqual("4,6,3,5,6,3,5,2,1,0");
+  });
+
+  Deno.test("Part 1 - adv", () => {
+    const input = `
+      Register A: 800
+      Register B: 0
+      Register C: 0
+
+      Program: 0,5
+    `
+      .replaceAll(/^ +/gm, "")
+      .trim();
+
+    expect(solvePart1(input).registers).toStrictEqual({
+      A: 800,
+      B: 0,
+      C: 0,
+    });
+  });
+
+  Deno.test("Part 1 - bxl", () => {
+    const input = `
+      Register A: 0
+      Register B: 777
+      Register C: 0
+
+      Program: 1,7
+    `
+      .replaceAll(/^ +/gm, "")
+      .trim();
+
+    expect(solvePart1(input).registers).toStrictEqual({
+      A: 0,
+      B: 782,
+      C: 0,
+    });
+  });
+
+  Deno.test("Part 1 - bst", () => {
+    const input = `
+      Register A: 0
+      Register B: 0
+      Register C: 9
+
+      Program: 2,6
+    `
+      .replaceAll(/^ +/gm, "")
+      .trim();
+
+    expect(solvePart1(input).registers).toStrictEqual({
+      A: 0,
+      B: 1,
+      C: 9,
+    });
+  });
+
+  Deno.test("Part 1 - jnz - should do nothing", () => {
+    const input = `
+      Register A: 0
+      Register B: 0
+      Register C: 9
+
+      Program: 3,6
+    `
+      .replaceAll(/^ +/gm, "")
+      .trim();
+
+    expect(solvePart1(input).registers).toStrictEqual({
+      A: 0,
+      B: 0,
+      C: 9,
+    });
+  });
+
+  Deno.test("Part 1 - jnz - should jump", () => {
+    const input = `
+      Register A: 1
+      Register B: 100
+      Register C: 0
+
+      Program: 3,4,2,6,1,6
+    `
+      .replaceAll(/^ +/gm, "")
+      .trim();
+
+    expect(solvePart1(input).registers).toStrictEqual({
+      A: 1,
+      B: 98,
+      C: 0,
+    });
+  });
+
+  Deno.test("Part 1 - bxc", () => {
+    const input = `
+      Register A: 0
+      Register B: 849
+      Register C: 911
+
+      Program: 4,3
+    `
+      .replaceAll(/^ +/gm, "")
+      .trim();
+
+    expect(solvePart1(input).registers).toStrictEqual({
+      A: 0,
+      B: 222,
+      C: 911,
+    });
+  });
+
+  Deno.test("Part 1 - bdv", () => {
+    const input = `
+      Register A: 800
+      Register B: 0
+      Register C: 0
+
+      Program: 6,5
+    `
+      .replaceAll(/^ +/gm, "")
+      .trim();
+
+    expect(solvePart1(input).registers).toStrictEqual({
+      A: 800,
+      B: 800,
+      C: 0,
+    });
+  });
+
+  Deno.test("Part 1 - cdv", () => {
+    const input = `
+      Register A: 800
+      Register B: 0
+      Register C: 0
+
+      Program: 7,5
+    `
+      .replaceAll(/^ +/gm, "")
+      .trim();
+
+    expect(solvePart1(input).registers).toStrictEqual({
+      A: 800,
+      B: 0,
+      C: 800,
+    });
+  });
+
+  Deno.test("Part 1 - Test program 1", () => {
+    const input = `
+      Register A: 10
+      Register B: 0
+      Register C: 0
+
+      Program: 5,0,5,1,5,4
+    `
+      .replaceAll(/^ +/gm, "")
+      .trim();
+
+    expect(solvePart1(input).outcome).toEqual("0,1,2");
+  });
+
+  Deno.test("Part 1 - Test program 2", () => {
+    const input = `
+      Register A: 2024
+      Register B: 0
+      Register C: 0
+
+      Program: 0,1,5,4,3,0
+    `
+      .replaceAll(/^ +/gm, "")
+      .trim();
+
+    expect(solvePart1(input).outcome).toEqual("4,2,5,6,7,7,7,7,3,1,0");
+    expect(solvePart1(input).registers.A).toEqual(0);
+  });
+
+  Deno.test("Part 1 - Test program 3", () => {
+    const input = `
+      Register A: 0
+      Register B: 29
+      Register C: 0
+
+      Program: 1,7
+      `
+      .replaceAll(/^ +/gm, "")
+      .trim();
+
+    expect(solvePart1(input).registers.B).toEqual(26);
+  });
+
+  Deno.test("Part 1 - Test program 4", () => {
+    const input = `
+      Register A: 0
+      Register B: 2024
+      Register C: 43690
+
+      Program: 4,0
+    `
+      .replaceAll(/^ +/gm, "")
+      .trim();
+
+    expect(solvePart1(input).registers.B).toEqual(44354);
+  });
+
+  // 404712716 not the right answer
+  Deno.test("Part 1 - File input", () => {
+    expect(solvePart1(fileInput).outcome).toEqual("");
+  });
+
+  Deno.test.ignore("Part 2 - Example input", () => {
+    expect(solvePart2(exampleInput)).toEqual(0);
+  });
+
+  Deno.test.ignore("Part 2 - File input", () => {
+    expect(solvePart2(fileInput)).toEqual(0);
+  });
+});
---
+++ 17_chronospatial-computer/index.ts
@@ -0,0 +1,182 @@
+import chalk from "chalk";
+
+const resolveComboOperand = (operand: number, registers: Registers) => {
+  if (operand <= 3) {
+    return operand;
+  }
+
+  return {
+    4: registers.A,
+    5: registers.B,
+    6: registers.C,
+  }[operand];
+};
+
+type Registers = {
+  A: number;
+  B: number;
+  C: number;
+};
+
+const adv = (comboOperand: number, registers: Registers) => {
+  const numerator = registers.A;
+  const denominator = resolveComboOperand(comboOperand, registers)!;
+  const result = numerator / 2 ** denominator;
+  registers.A = Math.floor(result);
+};
+
+const bxl = (literalOperand: number, registers: Registers) => {
+  registers.B = registers.B ^ literalOperand;
+};
+
+const bst = (comboOperand: number, registers: Registers) => {
+  const operand = resolveComboOperand(comboOperand, registers)!;
+  registers.B = operand % 8;
+};
+
+const jnz = (literalOperand: number, registers: Registers) => {
+  if (registers.A === 0) return;
+
+  return literalOperand;
+};
+
+const bxc = (operand: number, registers: Registers) => {
+  registers.B = registers.B ^ registers.C;
+};
+
+const out = (comboOperand: number, registers: Registers) => {
+  const operand = resolveComboOperand(comboOperand, registers)!;
+  return operand % 8;
+};
+
+const bdv = (comboOperand: number, registers: Registers) => {
+  const numerator = registers.A;
+  const denominator = resolveComboOperand(comboOperand, registers)!;
+  const result = numerator / 2 ** denominator;
+  registers.B = Math.floor(result);
+};
+
+const cdv = (comboOperand: number, registers: Registers) => {
+  const numerator = registers.A;
+  const denominator = resolveComboOperand(comboOperand, registers)!;
+  const result = numerator / 2 ** denominator;
+  registers.C = Math.floor(result);
+};
+
+type Opcode = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
+type ProgramName =
+  | "adv"
+  | "bxl"
+  | "bst"
+  | "jnz"
+  | "bxc"
+  | "out"
+  | "bdv"
+  | "cdv";
+
+export const Programs: Record<
+  Opcode,
+  { fn: (operand: number, registers: Registers) => void; name: ProgramName }
+> = {
+  0: {
+    fn: adv,
+    name: "adv",
+  },
+  1: {
+    fn: bxl,
+    name: "bxl",
+  },
+  2: {
+    fn: bst,
+    name: "bst",
+  },
+  3: {
+    fn: jnz,
+    name: "jnz",
+  },
+  4: {
+    fn: bxc,
+    name: "bxc",
+  },
+  5: {
+    fn: out,
+    name: "out",
+  },
+  6: {
+    fn: bdv,
+    name: "bdv",
+  },
+  7: {
+    fn: cdv,
+    name: "cdv",
+  },
+};
+
+const instructions = (
+  opcode: Opcode,
+  operand: number,
+  registers: Registers
+) => {
+  const program = Programs[opcode]!;
+  const outcome = program.fn(operand, registers);
+
+  return {
+    outcome,
+    programExecuted: program.name,
+  };
+};
+
+export const solvePart1 = (input: string) => {
+  const registerValues = [
+    ...input.trim().matchAll(/^Register [ABC]: (\d+)$/gm),
+  ].map((m) => Number(m[1]));
+  const program = [...input.trim().matchAll(/^Program: ((?:\d,)+\d)$/gm)][0][1]
+    .split(",")
+    .map((n) => Number(n)) as Opcode[];
+
+  const registers = {
+    A: registerValues[0],
+    B: registerValues[1],
+    C: registerValues[2],
+  };
+
+  let instructionPointer = 0;
+  let outcomeStream = "";
+
+  while (instructionPointer < program.length) {
+    const instruction = program[instructionPointer];
+    const operand = program[instructionPointer + 1];
+
+    const { outcome, programExecuted } = instructions(
+      instruction,
+      operand,
+      registers
+    );
+
+    // console.log("---- new loop! ----");
+    // console.log("[instruction]", instruction, "[operand]", operand);
+    // console.log("[programExecuted]", programExecuted, "[outcome]", outcome);
+    // console.log("registers", registers);
+
+    if (programExecuted === "out") {
+      outcomeStream += String(outcome) + ",";
+    }
+
+    if (programExecuted === "jnz") {
+      // In the case of an jnz, we know that the outcome is the indeex to jump to
+      instructionPointer = outcome!;
+      continue;
+    }
+
+    instructionPointer += 2;
+  }
+
+  return {
+    registers,
+    outcome: outcomeStream.slice(0, -1),
+  };
+};
+
+export const solvePart2 = (input: string) => {
+  return 0;
+};
