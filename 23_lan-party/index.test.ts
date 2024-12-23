9780e8aef2da243ae130ff3f1eeff4372bdc36bd
Kevin van der Werff <kevin.vanderwerff@deptagency.com>
2024-12-23 23:50:23 +0100

    WIP day 23 part 2

--- 23_lan-party/index.test.ts
+++ 23_lan-party/index.test.ts
@@ -55,9 +55,98 @@ testWrapper("Day 23", () => {
     expect(solvePart1(fileInput)).toEqual(926);
   });
 
-  Deno.test.ignore("Part 2 - Example input", () => {
-    expect(solvePart2(exampleInput)).toEqual(0);
+  Deno.test("Part 2 - Example input", () => {
+    expect(solvePart2(exampleInput)).toEqual("co,de,ka,ta");
   });
+
+  Deno.test("Part 2 - Example input - should join marked field", () => {
+    expect(
+      solvePart2(
+        `
+kh-tc
+qp-kh
+qp-tc
+`.trim()
+      )
+    ).toEqual("kh,qp,tc");
+  });
+
+  Deno.test(
+    "Part 2 - Example input - 2 distinct marked fields should merge",
+    () => {
+      expect(
+        solvePart2(
+          `
+kh-tc
+qp-ip
+kh-qp
+kh-ip
+tc-qp
+tc-ip
+`.trim()
+        )
+      ).toEqual("ip,kh,qp,tc");
+    }
+  );
+
+  Deno.test("Part 2 - Example input - Return largest marked field", () => {
+    expect(
+      solvePart2(
+        `
+a-b
+b-c
+x-y
+c-d
+a-c
+a-d
+b-d
+`.trim()
+      )
+    ).toEqual("a,b,c,d");
+  });
+
+  Deno.test(
+    "Part 2 - Example input - Merge 2 marked fields with 1 being bigger",
+    () => {
+      expect(
+        solvePart2(
+          `
+a-b
+b-c
+x-y
+c-d
+a-c
+a-d
+b-d
+x-a
+x-b
+x-c
+x-d
+y-a
+y-b
+y-c
+y-d
+`.trim()
+        )
+      ).toEqual("a,b,c,d,x,y");
+    }
+  );
+
+  Deno.test.only(
+    "Part 2 - Example input - Bigger field is formed from a part of a second field",
+    () => {
+      expect(
+        solvePart2(
+          `
+a-b
+c-d
+a-c
+a-d
+`.trim()
+        )
+      ).toEqual("a,c,d");
+    }
+  );
 
   Deno.test.ignore("Part 2 - File input", () => {
     expect(solvePart2(fileInput)).toEqual(0);
--- 23_lan-party/index.ts
+++ 23_lan-party/index.ts
@@ -19,7 +19,6 @@ class GraphNode {
 
 export const solvePart1 = (input: string) => {
   const nodes = new Map<string, GraphNode>();
-
   const startNodes = [] as GraphNode[];
 
   input
@@ -91,6 +90,99 @@ export const solvePart1 = (input: string) => {
   return circularPaths.size;
 };
 
+type MarkId = string;
+
 export const solvePart2 = (input: string) => {
-  return 0;
+  const nodes = new Map<string, GraphNode>();
+
+  const markPerNode = new Map<string, MarkId>();
+  const markedFields = new Map<MarkId, GraphNode[]>();
+
+  input
+    .trim()
+    .split("\n")
+    .forEach((line) => {
+      const [_, fst, snd] = line.match(/^(\w+)-(\w+)$/) ?? [];
+
+      if (!nodes.get(fst)) nodes.set(fst, new GraphNode(fst));
+      if (!nodes.get(snd)) nodes.set(snd, new GraphNode(snd));
+
+      const fstNode = nodes.get(fst)!;
+      const sndNode = nodes.get(snd)!;
+
+      fstNode.addUndirectedNeighbour(sndNode);
+
+      // Edge is between 2 completely new nodes
+      if (!markPerNode.has(fstNode.name) && !markPerNode.has(sndNode.name)) {
+        const markId = `${fstNode.name}-${sndNode.name}` as MarkId;
+
+        markPerNode.set(fstNode.name, markId);
+        markPerNode.set(sndNode.name, markId);
+
+        markedFields.set(markId, [fstNode, sndNode]);
+      }
+
+      // One of the nodes is in the markedFIelds, the other is not
+      if (
+        (markPerNode.has(fstNode.name) && !markPerNode.has(sndNode.name)) ||
+        (markPerNode.has(sndNode.name) && !markPerNode.has(fstNode.name))
+      ) {
+        const [alreadyMarkedNode, newNode] = markPerNode.has(fstNode.name)
+          ? [fstNode, sndNode]
+          : [sndNode, fstNode];
+
+        const markId = markPerNode.get(alreadyMarkedNode.name)!;
+        const allMarkedNodes = markedFields.get(markId)!;
+
+        // Check if the new incoming node has an edge to all marked nodes
+        if (
+          allMarkedNodes.every((markedNode) =>
+            markedNode.outgoing.some((node) => node.name === newNode.name)
+          )
+        ) {
+          // If so, we can add the new node to the markedFields
+          markedFields.set(markId, [...allMarkedNodes, newNode]);
+          markPerNode.set(newNode.name, markId);
+        }
+      }
+
+      // Both nodes are already marked, need to check if we need to merge them
+      if (markPerNode.has(fstNode.name) && markPerNode.has(sndNode.name)) {
+        const markId1 = markPerNode.get(fstNode.name)!;
+        const markId2 = markPerNode.get(sndNode.name)!;
+
+        const field1 = markedFields.get(markId1)!;
+        const field2 = markedFields.get(markId2)!;
+
+        const isAllConnected = field1?.every((node1) => {
+          return field2?.every((node2) =>
+            node2.outgoing.some(
+              (outgoingNode) => outgoingNode.name === node1.name
+            )
+          );
+        });
+
+        if (isAllConnected) {
+          markedFields.set(markId1, [...field1, ...field2]);
+          markedFields.delete(markId2);
+
+          field2.forEach((node) => {
+            markPerNode.set(node.name, markId1);
+          });
+        }
+      }
+    });
+
+  // console.log(markedFields);
+
+  return markedFields
+    .entries()
+    .reduce(
+      (acc: [MarkId, GraphNode[]], markedField: [MarkId, GraphNode[]]) => {
+        return acc[1].length > markedField[1].length ? acc : markedField;
+      }
+    )[1]
+    .map((node) => node.name)
+    .toSorted()
+    .join(",");
 };
